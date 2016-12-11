f428e20db5b6c4a70797a7cce54181ccded91dca
Omegamark <grant.mark@gmail.com>
2016-12-10 17:47:51 -0700

    Still need to finish reduce

--- filter/core.js
+++ filter/core.js
@@ -1,16 +1,46 @@
 function onlyEven (array) {
+  return array.filter(function(value) {
+    return value % 2 === 0;
+  })
+  }
   // your code here
-};
+
 
 function onlyOneWord (array) {
+  return array.filter(function(value) {
+    return !value.includes(' ');
+  })
   // your code here
 };
 
 function positiveRowsOnly (array) {
-  // your code here
+ function greaterThanZero(element){
+   return element >= 0;
+ }
+ array = array.filter((innerArray)=>{
+   if (innerArray.every(greaterThanZero)) {
+     return innerArray;
+   }
+   // for (var i = 0; i < innerArray.length; i++) {
+   //   if (innerArray[i] <= -1) {
+   //     return false;
+   //   }
+   // }
+   // return innerArray;
+ });
+ return array;
 };
+// positiveRowsOnly(matrix);
+//
+//
+//   })
+// }
+
+  // your code here
+
 
 function allSameVowels (array) {
+  
   // your code here
 };
 
--- map/core.js
+++ map/core.js
@@ -1,18 +1,80 @@
 function multiplyBy10 (array) {
- // your code here
+  return array.map(function(x) {
+    return x * 10;
+  })
 };
 
 function shiftRight (array) {
- // your code here
-};
+  let newArray = array.map(function(value, index) {
+      return array[(index + array.length - 1) % array.length]
+  })
+  return newArray
+}
+
+
+
+
+
 
 function onlyVowels (array) {
- // your code here
-};
+ let newArray2 = array.map(function(value) {
+   return value.match(/a|e|i|o|u/gi).join("");
+   //return value.replace(/a|e|i|o|u/gi, "");
+
+ })// your code here
+ console.log(newArray2);
+ return newArray2;
+}
+
+
+// function onlyVowels (array) {
+//  array.map(function(value) {
+//    return value.match(/a|e|i|o|u/gi).join("");
+//    //return value.replace(/a|e|i|o|u/gi, "");
+//
+//  })// your code here
+//
+//  return array;
+// }
+
+
+
+// function onlyVowels (array) {
+//  let newArray2 = array.map(function(value, index) {
+//    value.match(/a|e|i|o|u/gi).join("");
+//
+//    console.log(newArray2);
+//  })// your code here
+//  return newArray2;
+// }
+
+// var matrix = [[1,2,3],
+//               [4,5,6],
+//               [7,8,9]];
+
+
 
 function doubleMatrix (array) {
- // your code here
+  return array.map(function(value, index) {
+    return value.map(function(value, index){
+    return value * 2
+  })
+
+  });
 };
+
+
+// (function doubleMatrix (array) {
+//   let newArray3 = array.map(function(value, index) {
+//     value.map(function(value, index){
+//     return value * 2
+//   })
+//
+//   });
+//   console.log(newArray3);
+//   return newArray3;
+//  // your code here
+// }(matrix));
 
 module.exports = {
   multiplyBy10: multiplyBy10,
